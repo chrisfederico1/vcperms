50a5c323fd337f0416373676452c32bb2e0dfa3a
chrisfederico1 <34996684+chrisfederico1@users.noreply.github.com>
2019-11-18 10:57:32 -0600

    Add files via upload

---
+++ create-vcpermsreport.ps1
@@ -0,0 +1,37 @@
+
+
+<#
+    
+    
+#>
+
+function create-vcpermsreport
+
+{
+    
+# Parameters
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [string]$Server,
+
+        [Parameter(Mandatory=$false)]
+        [string]$Path
+    )
+
+BEGIN {
+    $permissions = @()
+
+}
+PROCESS{
+
+    $permissions += Get-VIPermission | Select-Object @{N='VCENTER';E={$_.Uid.split('@:')[1]}},Principal,Role,@{n='Entity';E={$_.Entity.Name}},@{N='Entity Type';E={$_.Entityid.split('-')[0]}}
+}
+
+END{
+    $permissions | export-csv .\vcpermsreport.csv -NoTypeInformation
+
+}
+
+}
